b41c46615f98656c10535dba8f35d1cb29aa665b
Aashish Jain <Aashj99@gmail.com>
2015-08-06 21:47:36 -0700

    holstein added

--- src/holstein.java
+++ src/holstein.java
@@ -1,6 +1,10 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 /*
@@ -13,12 +17,19 @@ public class holstein
     private static Scanner sc;
     private static PrintWriter pw;
     private static int[] minimumRequirements;
+    private static int[][] feeds;
+    private static int numFeeds;
+    private static int numRequirements;
+    private static HashMap<int[], ArrayList<Integer>> hm = new HashMap<int[],
+            ArrayList<Integer>>();
+    private static ArrayList<ArrayList<Integer>> answers = new
+            ArrayList<ArrayList<Integer>>();
     public static void main(String[] args)throws IOException
     {
         sc = new Scanner(new File("holstein.in"));
         pw = new PrintWriter(new File("holstein.out"));
 
-        int numRequirements = sc.nextInt();
+        numRequirements = sc.nextInt();
 
         minimumRequirements = new int[numRequirements];
 
@@ -27,9 +38,9 @@ public class holstein
             minimumRequirements[i] = sc.nextInt();
         }
 
-        int numFeeds = sc.nextInt();
+        numFeeds = sc.nextInt();
 
-        int[][] feeds = new int[numFeeds][numRequirements];
+        feeds = new int[numFeeds][numRequirements];
 
         for (int i = 0; i < feeds.length; i++)
         {
@@ -39,7 +50,85 @@ public class holstein
             }
         }
 
+        if (numFeeds == 1)
+        {
+            pw.println(numFeeds + " " + numFeeds);
+        }
+        else
+        {
+            solve(minimumRequirements, 0, new ArrayList<Integer>());
+
+            getBest(answers);
+        }
+
+        pw.close();
     }
+
+    public static void getBest(ArrayList<ArrayList<Integer>> ans)
+    {
+        ArrayList<Integer> best = ans.get(0);
+
+        for (int i = 1; i < ans.size(); i++)
+        {
+            ArrayList<Integer> thisArray = ans.get(i);
+            if (thisArray.size() < best.size())
+            {
+                best = thisArray;
+            }
+        }
+        pw.print(best.size() + " ");
+        for (int i = 0; i < best.size() - 1; i++)
+        {
+            pw.print(best.get(i) + " ");
+        }
+        pw.println(best.get(best.size() - 1));
+
+    }
+
+    public static void solve(int[] currentLeft, int currentScoop,
+            ArrayList<Integer> sb)
+    {
+        if (allRequirementsGood(currentLeft))
+        {
+            answers.add(sb);
+        }
+        else
+        {
+            for (int i = currentScoop + 1; i <= numFeeds; i++)
+            {
+                ArrayList<Integer> s = (ArrayList<Integer>) sb.clone();
+                hm.put(currentLeft, s);
+                sb.add(i);
+                solve(subtract(currentLeft, feeds[i - 1]), i, sb);
+                sb = hm.get(currentLeft);
+            }
+        }
+    }
+
+
+    public static boolean allRequirementsGood(int[] requirements)
+    {
+        for (int i = 0; i < requirements.length; i++)
+        {
+            if (requirements[i] > 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] subtract(int[] one, int[] two)
+    {
+        int[] answer = new int[one.length];
+        for (int i = 0; i < one.length; i++)
+        {
+            answer[i] = one[i] - two[i];
+        }
+        return answer;
+    }
+
+
 
 
 }
